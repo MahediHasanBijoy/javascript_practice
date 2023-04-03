245b138d9a2effdad323f5bdaec0ea01a6137f11
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-04-04 00:10:13 +0600

    added for of loop

--- 15_for_of_loop.js
+++ 15_for_of_loop.js
@@ -2,3 +2,26 @@
 
 // The JavaScript for of statement loops through the values of an iterable object.
 
+// Looping over an Array 🎈🎈
+const cars = ['BMW', 'Volvo', 'Audi'];
+
+let text = "";
+// here x is value of array
+for(let x of cars){
+    text += x;
+}
+
+console.log(text);
+
+
+
+// Looping over a String🎁🎁
+let language = "JavaScript";
+
+let line = "";
+
+for(let x of language){
+    line += x;
+}
+
+console.log(line);
